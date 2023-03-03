47aaf0b23f6825b2b9907e75dcfc4f5c1f076037
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-03-02 20:54:12 -0600

    Add files via upload

--- moTest.js
+++ moTest.js
@@ -17,3 +17,22 @@ I think the window is the global bc its well what you see and interact with
 everything under that is just local scope
     like <h1>*/
 /*Its also full of func that no one want to reconfig*/
+
+
+//console.log(global); this pulls up all the global func
+
+//this would be good for a cheap lil web attack
+
+global.setTimeout(() =>{//second in the stack, but its coded first
+    console.log('The timeout');
+}, 3000);
+
+setTimeout(() =>{//final output
+    console.log('Timeout Two Times');
+    clearInterval(int);
+}, 6000);
+
+const int = setInterval(() =>{//first in the stack repeats twices(like timer),Then three times
+    console.log('This is a repeat')
+}, 1000)//every second
+
