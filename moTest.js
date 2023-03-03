023c4a32bf23fb87809dbd9de1516cdde6a40e18
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-03-02 20:45:08 -0600

    Add files via upload

---
+++ moTest.js
@@ -0,0 +1,19 @@
+let name = 'jeri james';
+const idk = 'something';
+
+
+//alert("NotePad gang");
+
+console.log("Testing Testing");
+
+const wassam = (name) =>{
+   console.log(`Hello ${name}`);
+}
+
+wassam('Lucy');
+wassam('Buck');
+/*global obj when u pull this up in the console
+I think the window is the global bc its well what you see and interact with
+everything under that is just local scope
+    like <h1>*/
+/*Its also full of func that no one want to reconfig*/
